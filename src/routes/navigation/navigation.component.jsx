35cc6f62005a7acedfdcbe19aa140b74aa934d99
juri-boni <100702839+juri-boni@users.noreply.github.com>
2022-10-14 16:30:09 +0200

    fixed sign-in/sign-out

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -31,7 +31,9 @@ const Navigation = () => {
           <NavLink to="/shop">SHOP</NavLink>
 
           {currentUser ? (
-            <NavLink onClick={signOutUser}>SIGN OUT</NavLink>
+            <NavLink as="span" onClick={signOutUser}>
+              SIGN OUT
+            </NavLink>
           ) : (
             <NavLink to="/auth">SIGN IN</NavLink>
           )}
